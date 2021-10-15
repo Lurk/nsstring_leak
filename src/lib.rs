690f30d28c6fad233042e4fb4ce8dce7313bc1bd
Sergey Bargamon <sergey@bargamon.ru>
2021-10-15 21:17:31 +0200

    add const NSUTF8_STRING_ENCODING: usize
    as @madsmtm pointed out -  just plugging 4 in as the encoding is UB, integers are i32 by default while the expected parameter is NSStringEncoding (typedef of NSUInteger, roughly usize). You should define a variable/constant with an explicit type or use usize, otherwise msg_send! will infer the wrong type!

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//! reproducible memory leak at objc_foundation::NSString::as_str
+#![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
+
 use objc::rc::autoreleasepool;
 use objc::{msg_send, sel, sel_impl};
 use objc_foundation::{INSString, NSString};
@@ -5,41 +8,44 @@ use objc_id::Id;
 use std::ffi::CString;
 
 pub fn leak(str: &str) {
+    //! leaking example
     let nsstring = NSString::from_str(str);
     nsstring.as_str();
 }
 
 pub fn no_leak_autoreleasepool(str: &str) {
+    //! no leak with autorelease pool
     let nsstring = NSString::from_str(str);
     convert_with_autoreleasepool(nsstring);
 }
 
 pub fn no_leak_vec(str: &str) {
+    //! no leak with getCString
     let nsstring = NSString::from_str(str);
     convert_with_vec(nsstring);
 }
 
 pub fn convert_with_autoreleasepool(nsstring: Id<NSString>) -> String {
+    //! https://github.com/SSheldon/rust-objc-foundation/issues/15#issuecomment-943180585
     autoreleasepool(move || nsstring.as_str().to_owned())
 }
 
-/**
-Function that converts NSString to rust string through CString to prevent a memory leak.
-
-encoding:
-   4 = NSUTF8StringEncoding
-   https://developer.apple.com/documentation/foundation/1497293-string_encodings/nsutf8stringencoding?language=objc
+// https://developer.apple.com/documentation/foundation/1497293-string_encodings/nsutf8stringencoding?language=objc
+const NSUTF8_STRING_ENCODING: usize = 4;
+ 
+ /**
+Function that converts NSString to rust string with getCString to prevent a memory leak.
 
 getCString:
    Converts the string to a given encoding and stores it in a buffer.
    https://developer.apple.com/documentation/foundation/nsstring/1415702-getcstring
 
- */
+*/
 pub fn convert_with_vec(nsstring: Id<NSString>) -> String {
     let string_size: usize = unsafe { msg_send![nsstring, lengthOfBytesUsingEncoding: 4] };
     let mut buffer: Vec<u8> = vec![0_u8; string_size + 1];
     let is_success: bool = unsafe {
-        msg_send![nsstring, getCString:buffer.as_mut_ptr()  maxLength:string_size+1 encoding:4]
+        msg_send![nsstring, getCString:buffer.as_mut_ptr()  maxLength:string_size+1 encoding:NSUTF8_STRING_ENCODING]
     };
     if is_success {
         // before from_vec_with_nul can be used https://github.com/rust-lang/rust/pull/89292
